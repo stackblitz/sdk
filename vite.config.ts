2d06c42ff07523395d06d70d0d4e933b8bd6c8e4
Florens Verschelde <florens@fvsch.com>
2022-11-08 20:27:56 +0100

    Move to dedicated stackblitz/sdk repository

---
+++ vite.config.ts
@@ -0,0 +1,25 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  build: {
+    target: 'es2020',
+    outDir: 'bundles',
+    emptyOutDir: true,
+    minify: 'esbuild',
+    sourcemap: true,
+    lib: {
+      name: 'StackBlitzSDK',
+      entry: 'src/index.ts',
+      formats: ['cjs', 'es', 'umd'],
+      // Override the default file names used by Vite's “Library Mode”
+      // (https://vitejs.dev/guide/build.html#library-mode) for backwards
+      // compatibility with microbundle's output.
+      fileName: (format, entryName) => {
+        let suffix: string = '';
+        if (format === 'es') suffix = '.m';
+        if (format === 'umd') suffix = '.umd';
+        return `sdk${suffix}.js`;
+      },
+    },
+  },
+});
